08f7cf734c571f926203137b1da96022b0ca887f
adamklekowski <klekowskiadam@gmail.com>
2019-03-13 01:50:56 +0100

    v1.0

---
+++ lab2-array2D.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int **Array2D(int n_rows, int n_columns)
+{
+    int ** array = new int * [n_rows];
+
+    for (int i = 0; i<n_rows; i++)
+    array[i] = new int [n_columns];
+
+    for (int i = 0; i<n_rows; i++)
+    {
+        for (int j=0; j<n_columns; j++)
+        {
+            array[i][j]=i*n_columns+j+1;
+        }
+    }
+    return array;
+}
+
+void PrintingArray2D(int**array, int n_rows, int n_columns)
+{
+    for (int i = 0; i<n_rows; i++)
+    {
+        for (int j=0; j<n_columns; j++)
+        {
+            cout.width(3);
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void DeleteArray2D(int**array, int n_rows, int n_columns)
+{
+    for (int i = 0; i<n_rows; i++)
+        delete [] array[i];
+    delete [] array;    
+}
+
+int main()
+{
+    int n_row, n_columns;
+    cout << "Liczba wierszy: ";
+    cin >> n_row;
+    cout << "Liczba kolumn:";
+    cin >> n_columns;
+    int ** array2D=Array2D(n_row,n_columns);
+    PrintingArray2D(array2D, n_row, n_columns);
+    DeleteArray2D(array2D, n_row, n_columns);
+}
